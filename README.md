4d312c8f32e5ab1d3344bfa1eea5d9a508ca621b
B1tWhys <skyarnold1@me.com>
2026-03-11 19:14:50 -0700

    Implement everything

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # No-emoji
 
 I hate how LLMs insist on putting non-ascii characters in comments and strings.
-This is a super simple pre-commit hook (written in rust for *speed*) that rejects
-changes containing non-unicode characters.
+This is a super simple pre-commit hook  that rejects changes containing non-ascii characters.
+
+Ideally the specifics of the valid/invalid character sets would be configurable, but for now it's
+just based on whatever the rust [std::Char::is_ascii()](https://doc.rust-lang.org/std/primitive.char.html#method.is_ascii)
+function does.
+
 
 ## Installation
-TODO
+Put a file like this in your repo's `.pre-commit-config.yaml`:
+```yaml
+FIXME
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::process::{ExitCode, exit};
+use std::{env, fs::File, io::{BufRead, BufReader}, path::PathBuf};
+
+const RST: &'static str = "\x1b[m";
+const RED: &'static str = "\x1b[31m";
+
+fn main() -> Result<(), ExitCode> {
+    let has_errors = env::args().skip(1)
+        .map(|arg| !file_is_ascii(&arg.into())
+            .unwrap_or(false))
+        .any(|x| x);
+
+    if has_errors {
+        Err(ExitCode::FAILURE)
+    } else {
+        Ok(()) 
+    }
+}
+
+fn file_is_ascii(path: &PathBuf) -> io::Result<bool> {
+    let file = File::open(&path).inspect_err(|e| eprintln!("Failed to open file path: {path:?}: {}", e))?;
+    let reader = BufReader::new(file);
+    let mut is_ascii = true;
+
+    for (line_num, line) in reader.lines().enumerate() {
+        let line = line?;
+        let line_num = line_num + 1;
+        if let Some((col, bad_char)) = line.chars().enumerate().find(|(_, c)| !c.is_ascii()) {
+            eprintln!("{RED}Error:{RST} Non-ascii character {bad_char} found at: {path:?}:{line_num}:{col}");
+            is_ascii = false;
+        }
+    }
+
+    Ok(is_ascii)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn path_in_test_dir(rel_path: &str) -> PathBuf {
+        let test_dir: PathBuf = env!("CARGO_MANIFEST_DIR").into();
+        test_dir.join("test_files").join(rel_path)
+    }
+
+    #[test]
+    fn file_is_ascii_with_bogus_file_path() {
+        let test_file_path = path_in_test_dir("bogus_file_path.txt");
+        let actual = file_is_ascii(&test_file_path);
+        assert!(actual.is_err());
+    }
+
+    
+    #[test]
+    fn file_is_ascii_for_all_ascii_file() {
+        let test_file_path = path_in_test_dir("ascii_file.txt");
+        let actual = file_is_ascii(&test_file_path);
+        assert!(matches!(actual, Ok(true)));
+    }
+
+    #[test]
+    fn file_is_ascii_with_unicode() {
+        let test_file_path = path_in_test_dir("unicode_file.txt");
+        let actual = file_is_ascii(&test_file_path);
+        assert!(matches!(actual, Ok(false)));
+    }
 }
